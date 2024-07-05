eb50f81e742937d5e556ad7970e3750c0904357c
prashantStha308 <prashant.795424@cct.tu.edu.np>
2024-07-05 18:58:24 +0545

    day 5

---
+++ day 5/day5DOM.js
@@ -0,0 +1,24 @@
+/*
+Case:
+
+Camel Case = firstSecondThird
+
+Snake Case = first_second_third
+
+Kebab Case = first-second-third
+
+Pascal Case = FirstSecondThird
+
+*/
+
+
+console.log( "Hello World" );
+
+const h1 = document.getElementsByTagName( "h1" )[0];
+h1.textContent = "Hello World";
+
+const h3 = document.getElementsByTagName( "h3" )[0];
+h3.textContent = "Bye";
+
+const test = document.getElementById("test");
+test.textContent = "Hello World";
--- day 5/script5.js
+++ day 5/script5.js
@@ -1,1 +1,69 @@
-console.log( "Awaiting Day 5" );
+// Day 3 of 7
+// JavaScript Workshop by Code for change and ITA.
+
+// Redude Higher order function
+// Reduce is used to perform calculation in numeric datas
+
+const num = [ 1,2,3,4,5 ];
+
+// reduce takes in two values. First one is called accumulator and the second one is the value we want to pass
+
+ const sum = num.reduce( ( accumulator , num )=>{ 
+
+    return num + accumulator;
+
+},0 );
+
+
+console.log(sum);
+
+const products = [
+    {
+        product : 'A',
+        price : 200,
+        qty : 2
+    },
+    {
+        product : 'B',
+        price : 300,
+        qty : 7
+    },
+    {
+        product : 'A',
+        price : 1100,
+        qty : 8
+    },
+]
+
+const totalPrice = products.reduce( ( acc , key )=>{
+    acc.totalPrice = key.price * key.qty + acc.totalPrice;
+    acc.totalQty = key.qty + acc.totalQty;
+    return acc;
+}, {totalPrice: 0, totalQty : 0});
+
+console.log(totalPrice);
+
+const reviews = [
+    {
+        rating : 2
+    },
+    {
+        rating : 4
+    },
+    {
+        rating : 5
+    },
+]
+
+const calcAvg = function( reviews ){
+    const totalRating = reviews.reduce( ( acc , key )=>{
+        return key.rating + acc;
+    },0 );
+    const average = totalRating/reviews.length;
+    console.log(average);
+}
+calcAvg(reviews);
+
+
+// DOM = Document Object Model
+
