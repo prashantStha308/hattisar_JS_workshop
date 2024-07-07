06c251bfe83b1d54263b8b9987b245b59eb98148
prashantStha308 <prashant.795424@cct.tu.edu.np>
2024-07-07 22:01:35 +0545

    Added comments to describe what each function does

--- todo_project/script.js
+++ todo_project/script.js
@@ -1,5 +1,10 @@
+// Script for toDo app
+
 var tasks = []; // An arrray that will contain user's lists in an object form
 
+// The function below is triggered when the Add button is clicked.
+// The addTas() function adds an object containing the task and the information that confirms if a task has been completed
+// After a task is added, the function calls another function to update the list
 function addTask(){
     var taskInput = document.getElementById('todoInput');
     var taskValue = taskInput.value;
@@ -14,6 +19,14 @@ function addTask(){
         updateTodoList();
     }
 }
+
+
+// The function below is invoked by different functions. The main task of the updateTodoList() is, as the name
+// suggests, to update the list in the webpage.
+// This function creates a new HTML element and adds the tasks present in the tasks array.
+// When any element created by this function is clicked, toggleCompleted() function
+// is invoked which takes which element is clicked as it's arguement
+// At the end of this function, updateAggregrate() function is invoked
 
 function updateTodoList(){
     const todoList = document.getElementById('todoList');
@@ -33,11 +46,23 @@ function updateTodoList(){
     updateAggregrate();
 }
 
+// The toggleCompleted() function is called inside addTask() function.
+// This function is invoked when a list element is clicked.
+// It changes the "complete" state of the passed object to it's opposite.
+// which mean, if the complete state is false, it makes it true and hence the completed
+// task will have a strikethrough
+
+// this function calls the updateTodoList() function again to upadate the list
+
 function toogleCompleted(task){
     task.completed = !task.completed;
     updateTodoList();
-
 }
+
+// The updateAggregrate() function updates the number of tasks and completed tasks count.
+// This function makes use of the Reduce Higher order function to perform
+// the arithmetic calculation for completed tasks count
+// For the total tasks count, we simply take the length of the array that contains the tasks
 
 function updateAggregrate(){
     var totalTasks = document.getElementById('totalTasks');
@@ -51,6 +76,12 @@ function updateAggregrate(){
     completedTasks.textContent = completed;
 }
 
+// filterTasks() function is used by the task searching feature.
+// This function makes use of the Filter Higher order function to search for the
+// tasks that matches the typed letters in the input field with id 'searchInput'
+// After being filtered, the updateTodoListWithFilteredTasks() function is called that
+// takes the filtered tasks as an arguement and updates the list
+
 function filterTasks(){
     var searchInput = document.getElementById('searchInput');
     var searchValue = searchInput.value.toLowerCase();
@@ -62,6 +93,12 @@ function filterTasks(){
     // update todolist with filteredTask 
     updateTodoListWithFilteredTasks(filteredTasks);
 }
+
+// updateTodoListWithFilteredTasks() function is invoked inside filterTasks() function
+// This function does the exact work as the updateTodoList() function but only
+// for the filtered tasks.
+// This again calls updateAggregrate() function to calculate the total tasks and
+// completed tasks
 
 function updateTodoListWithFilteredTasks(filteredTasks){
     var todoList = document.getElementById('todoList');
@@ -80,19 +117,16 @@ function updateTodoListWithFilteredTasks(filteredTasks){
     updateAggregrate();
 }
 
+// The reset() function is triggered when the Clear All button is clicked,
+// This function removes all the tasks from the list and clears the count for total tasks 
+// and completed tasks by invoking updateAggregrate() function
+
 const reset = ()=>{
 
     var todoList = document.getElementById('todoList');
     todoList.innerHTML = "";
+    tasks = []; // makes the array empty
 
-    tasks = [];
-
-    var totalTasks = document.getElementById('totalTasks');
-    var completedTasks = document.getElementById('completedTasks');
-
-    totalTasks.textContent = 0;
-    completedTasks.textContent = 0;
-
-    
+    updateAggregrate();
 
 }
